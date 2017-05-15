124888b6c78804ef28f440061912a9aff33424fb
Tyrel Cameron <camerontyrel@gmail.com>
2017-05-16 07:12:28 +1000

    Changing content variable names to match new names

--- source/content.js
+++ source/content.js
@@ -7,24 +7,24 @@ let userOptions;
 let headers;
 
 let process = () => {
-  if (userOptions.autoLevel.enabled) autoLevel();
-  if (userOptions.autoCast.enabled) autoCast();
-  if (userOptions.autoGems.enabled) autoGems();
-  if (userOptions.autoArmoire.enabled) autoArmoire();
-  if (userOptions.autoFeed.enabled) autoFeed();
-  if (userOptions.autoQuest.enabled) autoQuest();
-  if (userOptions.autobuyArmoire && userData.stats.gp > 100) buyArmoire();
+  console.log(userOptions);
+  if (userOptions.autoLevel && userOptions.autoLevel.enabled) autoLevel();
+  if (userOptions.autoCast && userOptions.autoCast.enabled) autoCast();
+  if (userOptions.autoGems && userOptions.autoGems.enabled) autoGems();
+  if (userOptions.autoArmoire && userOptions.autoArmoire.enabled) autoArmoire();
+  if (userOptions.autoFeed && userOptions.autoFeed.enabled) autoFeed();
+  if (userOptions.autoQuest && userOptions.autoQuest.enabled) autoQuest();
 };
 
 let autoArmoire = () => {
+  if (user.stats.gp < 100) return;
   axios
     .post("https://habitica.com/api/v3/user/buy-armoire", {}, { headers })
     .then(res => {
       if (res.status === 200) {
+        console.log("Bought Armoire");
         userData.stats.gp -= 100;
-        if (userData.stats.gp > 300) {
-          autoArmoire();
-        }
+        autoArmoire();
       }
     });
 };
@@ -32,13 +32,19 @@ let autoArmoire = () => {
 let autoCast = () => {
   // Well this is annoying. We have to do manual string concat because axios
   // is mutating the params string somehow and I can't quite diagnose it
-  if (userOptions.autoCast.spell.id === undefined) return;
-  let url = `https://habitica.com/api/v3/user/class/cast/${userOptions.autocastSpellValue}`;
+  if (userOptions.autoCast.spell.id === undefined) {
+    console.log("No spell ID set, exiting");
+    return;
+  }
+  let url = `https://habitica.com/api/v3/user/class/cast/${userOptions.autoCast.spell.id}`;
   if (userOptions.autoCast.task.id) {
-    url = `${url}?targetId=${userOptions.autocastTaskValue}`;
+    url = `${url}?targetId=${userOptions.autoCast.task.id}`;
   }
   axios.post(url, {}, { headers }).then(res => {
-    if (res.status === 200) autoCast();
+    if (res.status === 200) {
+      console.log(`Cast ${userOptions.autoCast.spell.name}`);
+      autoCast();
+    }
   }, console.log);
 };
 
@@ -51,6 +57,7 @@ let autoLevel = () => {
   let url = `https://habitica.com/api/v3/user/allocate?stat=${userOptions.autoLevel.id}`;
   axios.post(url, {}, { headers }).then(res => {
     if (res.status === 200) {
+      console.log(`Allocated Stat Point to ${userOptions.autoLevel.name}`);
       userData.stats.points--;
       if (userData.stats.points > 0) autoLevel();
     }
@@ -73,7 +80,10 @@ chrome.storage.sync.get(
   items => {
     userID = items.userID;
     apiToken = items.apiToken;
-    if (userID === undefined || apiToken === undefined) return;
+    if (userID === undefined || apiToken === undefined) {
+      console.log("No User ID and/or API Token, exiting.");
+      return;
+    }
 
     userOptions = items;
     headers = {
