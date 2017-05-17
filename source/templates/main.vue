3edb6dfb7978ab3ca6622e83284c9cfec6e146e2
Tyrel Cameron <camerontyrel@gmail.com>
2017-05-17 22:55:11 +1000

    Added spell costs so autocast will try to be smarter

--- source/templates/main.vue
+++ source/templates/main.vue
@@ -123,96 +123,112 @@ export default {
           value: "fireball",
           label: "Burst of Flames",
           image: "images/skill_fireball.png",
+          cost: 10,
           requireID: true
         },
         {
           value: "mpHeal",
           label: "Ethereal Surge",
           image: "images/skill_mpHeal.png",
+          cost: 30,
           requireID: false
         },
         {
           value: "earth",
           label: "Earthquake",
           image: "images/skill_earth.png",
+          cost: 35,
           requireID: false
         },
         {
           value: "frost",
           label: "Chilling Frost",
           image: "images/skill_frost.png",
+          cost: 40,
           requireID: false
         },
         {
           value: "smash",
           label: "Brutal Smash",
           image: "images/skill_smash.png",
+          cost: 10,
           requireID: true
         },
         {
           value: "defensiveStance",
           label: "Defensive Stance",
           image: "images/skill_defensiveStance.png",
+          cost: 25,
           requireID: false
         },
         {
           value: "valorousPresence",
           label: "Valorous Presence",
           image: "images/skill_valorousPresence.png",
+          cost: 20,
           requireID: false
         },
         {
           value: "intimidate",
           label: "Intimidating Gaze",
           image: "images/skill_intimidate.png",
+          cost: 15,
           requireID: false
         },
         {
           value: "pickPocket",
           label: "Pickpocket",
           image: "images/skill_pickPocket.png",
+          cost: 10,
           requireID: true
         },
         {
           value: "backStab",
           label: "Backstab",
           image: "images/skill_backStab.png",
+          cost: 15,
           requireID: true
         },
         {
           value: "toolsOfTrade",
           label: "Tools of the Trade",
           image: "images/skill_toolsOfTrade.png",
+          cost: 25,
           requireID: false
         },
         {
           value: "stealth",
           label: "Stealth",
           image: "images/skill_stealth.png",
+          cost: 45,
           requireID: false
         },
         {
           value: "heal",
           label: "Healing Light",
           image: "images/skill_heal.png",
+          cost: 15,
           requireID: false
         },
         {
           value: "protectAura",
           label: "Protective Aura",
           image: "images/skill_protectAura.png",
+          cost: 30,
           requireID: false
         },
         {
           value: "brightness",
           label: "Searing Brightness",
           image: "images/skill_brightness.png",
+          cost: 15,
           requireID: false
         },
         {
           value: "healAll",
           label: "Blessing",
           image: "images/skill_healAll.png",
+          cost: 25,
           requireID: false
         }
       ]
@@ -274,7 +290,8 @@ export default {
           items.autoCast.spell = {
             index: 1,
             id: this.spells[1].value,
-            name: this.spells[1].label
+            name: this.spells[1].label,
+            cost: this.spells[1].cost
           };
           items.autoCast.task = {
             id: "",
@@ -364,6 +381,7 @@ export default {
             obj.autoCast.spell.index = i;
             obj.autoCast.spell.id = this.spells[i].value;
             obj.autoCast.spell.name = this.spells[i].label;
+            obj.autoCast.spell.cost = this.spells[i].cost;
             chrome.storage.sync.set(obj);
           });
           return;
