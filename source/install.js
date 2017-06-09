86a6272d273c4cb76d00dbebb339259e5c31fc65
Tyrel Cameron <camerontyrel@gmail.com>
2017-06-10 05:54:45 +1000

    Install script now adds an entry to the log.

--- source/install.js
+++ source/install.js
@@ -2,5 +2,18 @@
 chrome.runtime.onInstalled.addListener(function(details) {
   if (details.reason == "install") {
     chrome.runtime.openOptionsPage();
+  } else if (details.reason == "update") {
+    chrome.storage.sync.get(["logs"], items => {
+      const c = new Date();
+      const datetime = `${c.getDate()} ${c.getHours()}:${c.getMinutes() < 10
+        ? "0"
+        : ""}${c.getMinutes()}`;
+      const manifest = chrome.runtime.getManifest();
+      if (manifest.version === undefined) return;
+      if (items.logs === undefined) items.logs = [];
+      const message = `Magic Wand updated to version ${manifest.version}`;
+      items.logs.push({ datetime, message });
+      chrome.storage.sync.set(items);
+    });
   }
 });
